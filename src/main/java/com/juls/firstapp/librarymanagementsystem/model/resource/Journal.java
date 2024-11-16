8934a1032597ea91d8cf833f237c14abab503762
adjetey-sowah <julius.sowah@amalitech.com>
2024-11-16 15:53:09 +0000

    Done adding other files

--- src/main/java/com/juls/firstapp/librarymanagementsystem/model/resource/Journal.java
+++ src/main/java/com/juls/firstapp/librarymanagementsystem/model/resource/Journal.java
@@ -1,4 +1,29 @@
 package com.juls.firstapp.librarymanagementsystem.model.resource;
 
-public class Journal {
+import com.juls.firstapp.librarymanagementsystem.model.enums.ResourceType;
+
+public class Journal extends LibraryResource{
+    private String issueNumber;
+    private String frequency;
+
+    public Journal(String title, String issueNumber, String frequency){
+        super(title,ResourceType.JOURNAL);
+        this.issueNumber = issueNumber;
+        this.frequency = frequency;
+    }
+
+    public Journal(String title) {
+        super(title, ResourceType.JOURNAL);
+    }
+
+    public Journal(){
+        super("",ResourceType.JOURNAL);
+    }
+
+    @Override
+    public void setResourceType() {
+        super.resourceType = ResourceType.JOURNAL;
+    }
+
+
 }
