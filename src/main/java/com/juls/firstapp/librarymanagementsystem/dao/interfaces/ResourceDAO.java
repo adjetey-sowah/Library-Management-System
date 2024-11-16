c02ea59a3d17964584cebe8e118e6ea82160f44f
adjetey-sowah <julius.sowah@amalitech.com>
2024-11-16 15:49:40 +0000

    creating the methods for the ResourceRepository

--- src/main/java/com/juls/firstapp/librarymanagementsystem/dao/interfaces/ResourceDAO.java
+++ src/main/java/com/juls/firstapp/librarymanagementsystem/dao/interfaces/ResourceDAO.java
@@ -1,6 +1,11 @@
 package com.juls.firstapp.librarymanagementsystem.dao.interfaces;
 
+import com.juls.firstapp.librarymanagementsystem.model.resource.LibraryResource;
+
 public interface ResourceDAO {
+
+    int addLibraryResource(LibraryResource resource) throws Exception;
+
 
 
 
